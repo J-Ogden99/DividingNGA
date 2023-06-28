19e0f80383901a0c0b1e2fca967d42a835a7b5fb
Josh Ogden <77122008+J-Ogden99@users.noreply.github.com>
2023-06-28 15:13:36 -0600

    Add docstring, add max_size parameter

--- Division.py
+++ Division.py
@@ -8,7 +8,22 @@ import pandas as pd
 from AdjoinAsGraph import create_adjoint_dict
 
 
-def divide_nga(stream_file: str, out_dir: str, code: str, region: str, n: int = 10, adjoint_tree: str = None):
+def divide_nga(stream_file: str, out_dir: str, region: str, n: int = 10, max_size: int = None, adjoint_tree: str = None):
+    """
+    Splits a network file from the NGA delineation into smaller pieces and writes them to parquets, for ease of
+    computation. Pieces are composed of complete adjoint basins, but the constituent basins don't necessarily border
+    each other geographically.
+    Args:
+        stream_file (str): Path to network file. Can be .csv, .parquet, .shp, or .gpkg.
+        out_dir (str): Path to directory which will contain the output pieces.
+        region (str): Unique region/VPU name or id, to be used in naming output files
+        n (int, optional): Number of parts to break region into. Defaults to 10.
+        max_size (int, optional): If defined, gives a maximum number of stream segments that a piece will contain,
+                                  and n is recalculated based on this number. Defaults to None.
+        adjoint_tree (str, optional): If defined, gives the path to pre-made dictionary created from AdjoinAsGraph and
+                                      stored in .json format. Otherwise this dictionary will be created during the
+                                      course of the run. Defaults to None.
+    """
     # Read file if of acceptable type
     ext = os.path.splitext(stream_file)[1]
     if ext == ".shp" or ext == ".gpkg":
@@ -30,22 +45,25 @@ def divide_nga(stream_file: str, out_dir: str, code: str, region: str, n: int = 
 
     # Create TERMINALPA column, get basin sizes
     drain_to_ocean = streams[streams['DSLINKNO'] == -1]['LINKNO'].tolist()
-    streams['TERMINALPA'] = -1
     full_basins = {}
     basin_sizes = {}
     for id in drain_to_ocean:
         full_basins[str(id)] = streams_tree[str(id)]
-        streams.loc[streams['LINKNO'].isin(streams_tree[str(id)]), 'TERMINALPA'] = id
         basin_sizes[str(id)] = len(full_basins[str(id)])
 
     # Add adjoint basin size to df, sort by that
+    if max_size:
+        n = int(len(streams) / max_size) + 1
     region_lists = [[] for i in range(n)]
     terminal_at = {}
-    streams['AdjSize'] = streams['TERMINALPA'].apply(lambda x: basin_sizes[str(x)] if x != -1 else 1)
+    streams['AdjSize'] = \
+        streams.apply(
+            lambda row: basin_sizes[str(int(row['TerminalNode']))] if int(row['TerminalNode']) != int(
+                row['LINKNO']) else 1, axis=1)
     streams = streams.sort_values('AdjSize', ascending=False).reset_index(drop=True)
     for i in range(len(streams)):
-        link_no = streams.loc[i, 'LINKNO']
-        t_pa = streams.loc[i, 'TERMINALPA']
+        link_no = int(streams.iloc[i]['LINKNO'])
+        t_pa = int(streams.iloc[i]['TerminalNode'])
         region_no = i % n
         if t_pa == -1:
             region_lists[region_no].append(link_no)
@@ -69,9 +87,5 @@ def divide_nga(stream_file: str, out_dir: str, code: str, region: str, n: int = 
     os.makedirs(out_dir)
 
     for i, lst in enumerate(region_lists):
-        streams.loc[streams['LINKNO'].isin(lst)].to_parquet(os.path.join(out_dir, f'{region}_{code}_{i + 1}.parquet'))
+        streams.loc[streams['LINKNO'].isin(lst)].to_parquet(os.path.join(out_dir, f'{region}_{i + 1}.parquet'))
         print(f'{region} {i + 1} Size: {len(lst)}')
-
-    # gdf = gpd.read_file(gdf_path)
-    # gdf['TERMINALPA'] = streams['TERMINALPA']
-    # gdf.to_file(f'TDX_streamreach_basins_{code}.gpkg', driver="GPKG")
--- Investigation.py
+++ Investigation.py
@@ -1,11 +1,14 @@
 import os
 
 from Division import divide_nga
+from AdjoinAsGraph import create_adjoint_dict
 
 code = '7020038340'
-region = '70s_northamerica'
+region = '50s_australia'
 out_dir = f'{region}_divided'
-stream_gpkg = os.path.join('DividingNGA', f'tdxhydro_streams_{region}', f'TDX_streamnet_{code}_01.gpkg')
+tree_path = f'{region}_tree.json'
+stream_file = os.path.join('DividingNGA', f'tdxhydro_streams_{region}', f'rapid_inputs_master.parquet')
 # gdf_path = os.path.join('DividingNGA', f'tdxhydro_basins_{region}', f'TDX_streamreach_basins_{code}_01.gpkg')
 
-divide_nga(stream_gpkg, out_dir, code, region)
+# create_adjoint_dict(stream_file, out_file=tree_path)
+divide_nga(stream_file, out_dir, region, max_size=50_000, adjoint_tree=tree_path)
